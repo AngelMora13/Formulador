6cc0b900c8a6c2baaec850885ead00c730404759
Angel Mora <angelmora13@hotmail.com>
2021-01-09 17:00:10 -0400

    cambios en db

--- ApiRestDj/settings.py
+++ ApiRestDj/settings.py
@@ -93,19 +93,13 @@ WSGI_APPLICATION = 'ApiRestDj.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
-if config('DJANGO_PRODUCTION',default=False, cast=bool):
-    DATABASE = {
+
+DATABASE = {
     'default': dj_database_url.config(
         default=config('DATABASE_URL')
     )
 }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME':os.path.join(BASE_DIR, 'db.sqlite3'),
-        }
-    }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
