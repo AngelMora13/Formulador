32cec528ffe6c65c7fa771def564afc1119fcfd5
Angel Mora <angelmora13@hotmail.com>
2021-01-28 16:38:32 -0400

    cambio email_backend fuera del if

--- ApiRestDj/settings.py
+++ ApiRestDj/settings.py
@@ -113,7 +113,8 @@ else:
             'ENGINE': 'django.db.backends.sqlite3',
             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }}
-    EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
+
+EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
 
 RECAPTCHA_SECRET_KEY=config("RECAPTCHA_SECRET_KEY")
     
